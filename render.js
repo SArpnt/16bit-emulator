268def7bf6e4d967f165b44f8bca12ae52eddd70
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-07-08 23:31:11 -0400

    formatting

--- render.js
+++ render.js
@@ -1,25 +1,25 @@
-"use strict"
-const fullarea = $('#fullarea')[0].getContext('2d')
-const display = $('#display')[0].getContext('2d')
-var mod = (x, y, s = 1) => (x % y + y * s) % y
+"use strict";
+const fullarea = $('#fullarea')[0].getContext('2d');
+const display = $('#display')[0].getContext('2d');
+var mod = (x, y, s = 1) => (x % y + y * s) % y;
 
 function newTileImage(pallete, image, bpp) {
-	let tile = $('<canvas width="8" height="8"></canvas>')[0].getContext('2d')
+	let tile = $('<canvas width="8" height="8"></canvas>')[0].getContext('2d');
 	for (let y = 0; y < 4 * bpp; y++)
 		for (let x = 0; x < 16 / bpp; x++) {
-			let color = (image[y] >> (x * bpp)) % (1 << bpp)
-			if (!color) continue
-			tile.fillStyle = pallete[color]
-			let p = x + (y * 16 / bpp)
-			tile.fillRect(p % 8, p >> 3, 1, 1)
+			let color = (image[y] >> (x * bpp)) % (1 << bpp);
+			if (!color) continue;
+			tile.fillStyle = pallete[color];
+			let p = x + (y * 16 / bpp);
+			tile.fillRect(p % 8, p >> 3, 1, 1);
 		}
-	return tile
+	return tile;
 }
 var palletes = [
 	[, '#000', '#fff', '#f00', '#ff0', '#0f0', '#0ff', '#00f', '#f0f', '#000', '#000', '#000', '#000', '#000', '#000', '#000'],
 	[, '#800', '#080', '#008'],
 	[, '#000']
-]
+];
 
 var tilemap = [
 	newTileImage(palletes[0], [
@@ -44,9 +44,9 @@ var tilemap = [
 		0b0100001001111100,
 		0b0000000001000010,
 	], 1),
-]
+];
 
-fullarea.clearRect(0, 0, 512, 512)
+fullarea.clearRect(0, 0, 512, 512);
 
 for (let y = 0; y < 64; y++) {
 	for (let x = 0; x < 64; x++) {
@@ -54,23 +54,23 @@ for (let y = 0; y < 64; y++) {
 			tilemap[(x + y) % 4].canvas,
 			x * 8,
 			y * 8
-		)
+		);
 	}
 }
-var draw
+var draw;
 {
-	let lnow = 0
+	let lnow = 0;
 	draw = function () {
-		let now = performance.now()
-		$('#fps')[0].innerHTML = Math.round(1000/(now-lnow))
-		lnow = now
+		let now = performance.now();
+		$('#fps')[0].innerHTML = Math.round(1000 / (now - lnow));
+		lnow = now;
 
-		display.clearRect(0, 0, 480, 360) 
+		display.clearRect(0, 0, 480, 360);
 
 		for (let y = 0; y < 360; y++) {
-			let yo = Math.floor(y*(Math.sin(now/100)-1)/5)
-			let xo = Math.floor(now / 100 * (yo+y>>3)-180)
-			let i = mod(xo, 512, -1)
+			let yo = Math.floor(y * (Math.sin(now / 100) - 1) / 5);
+			let xo = Math.floor(now / 100 * (yo + y >> 3) - 180);
+			let i = mod(xo, 512, -1);
 			for (let w = 0; w < 1 + (i < -32); w++)
 				display.drawImage(
 					fullarea.canvas,
@@ -82,24 +82,24 @@ var draw
 					y,
 					512,
 					1
-				)
+				);
 		}
-		requestAnimationFrame(draw)
-	}
+		requestAnimationFrame(draw);
+	};
 }
-requestAnimationFrame(draw)
+requestAnimationFrame(draw);
 
 function toYUV(R, G, B) {
-	let Y = 0.257 * R + 0.504 * G + 0.098 * B
-	let U = -0.148 * R - 0.291 * G + 0.439 * B
-	let V = 0.439 * R - 0.368 * G - 0.071 * B
-	return [Y, U, V]
+	let Y = 0.257 * R + 0.504 * G + 0.098 * B;
+	let U = -0.148 * R - 0.291 * G + 0.439 * B;
+	let V = 0.439 * R - 0.368 * G - 0.071 * B;
+	return [Y, U, V];
 }
 function toRGB(Y, U, V) {
-	let R, G, B
-	R = G = B = 1.164 * Y
-	R += 1.596 * V
-	G -= 0.392 * U - 0.813 * V
-	B += 2.017 * U
-	return [R, G, B]
+	let R, G, B;
+	R = G = B = 1.164 * Y;
+	R += 1.596 * V;
+	G -= 0.392 * U - 0.813 * V;
+	B += 2.017 * U;
+	return [R, G, B];
 }
